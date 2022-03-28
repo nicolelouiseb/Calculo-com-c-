4240dbb035048e88ccbe793070d009376fc5a10f
nicolelouiseb <101573165+nicolelouiseb@users.noreply.github.com>
2022-03-28 14:27:04 -0300

    Update and rename Program.cs to AumentoDe10Porcento.cs

--- AumentoDe10Porcento.cs
+++ AumentoDe10Porcento.cs
@@ -3,4 +3,4 @@ double aumento=0.10;
 Console.WriteLine("Insira o valor do produto:");
 valori=double.Parse(Console.ReadLine());
 valorf=valori-(valori*aumento);
-Console.WriteLine("O valor final após o desconto é de:{0}",valorf);
+Console.WriteLine("O valor final após o aumento é de:{0}",valorf);
