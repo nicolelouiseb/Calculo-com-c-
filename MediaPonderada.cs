61ef7641422472190e8ae242fdf440f039bbc8fc
nicolelouiseb <101573165+nicolelouiseb@users.noreply.github.com>
2022-03-28 14:17:51 -0300

    Update and rename Program.cs to MediaPonderada.cs

---
+++ MediaPonderada.cs
@@ -0,0 +1,10 @@
+﻿
+float n1,p1,n2,p2, result;
+  Console.WriteLine("Insira a nota1:");
+    n1=float.Parse(Console.ReadLine());
+  Console.WriteLine("Insira a nota2:");
+    n2=float.Parse(Console.ReadLine());
+p1=2;
+p2=3;
+result=(p1*n1+p2*n2)/(p1+p2);
+  Console.WriteLine("A media ponderada das notas é: {0}", result);
