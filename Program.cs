fa960afacb29a8ecccc5293485e8de05005c0e80
nicolelouiseb <101573165+nicolelouiseb@users.noreply.github.com>
2022-03-28 14:20:34 -0300

    Update and rename Program.cs to SalariosMinimosRecebidos.cs

--- Program.cs
+++
@@ -1,9 +0,0 @@
-﻿double salM, salF, result;
-Console.WriteLine("Insira o valor do salario mínimo:");
-salM=double.Parse(Console.ReadLine());
-
-Console.WriteLine("Insira o valor do salario do funcionario:");
-salF=double.Parse(Console.ReadLine());
-result=salF/salM;
-Console.WriteLine("A quantidade de salarios minimos recebidos pelo funcionário é de: {0}", result);
-
