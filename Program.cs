deda74c370fd2c1a4eaa9b409546e5972a7a96a6
nicolelouiseb <101573165+nicolelouiseb@users.noreply.github.com>
2022-03-28 14:26:12 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿double valori,valorf;
+double aumento=0.10;
+Console.WriteLine("Insira o valor do produto:");
+valori=double.Parse(Console.ReadLine());
+valorf=valori-(valori*aumento);
+Console.WriteLine("O valor final após o desconto é de:{0}",valorf);
