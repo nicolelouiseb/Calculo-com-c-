1c97149a5d42b4b0556b191a06cefddb3e2ea3ad
nicolelouiseb <101573165+nicolelouiseb@users.noreply.github.com>
2022-03-28 14:16:52 -0300

    Add files via upload

---
+++ ex1.cs
@@ -0,0 +1,9 @@
+﻿
+float n1, n2, result;
+Console.WriteLine("Programa para subtração de dois numeros");
+Console.WriteLine("Escreva o numero 1");
+n1= float.Parse(Console.ReadLine());
+Console.WriteLine("Escreva o n2");
+n2= float.Parse(Console.ReadLine());
+result= n1-n2;
+Console.WriteLine("O resultado da subtração é: {0}", result);
